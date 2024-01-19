1cf6b6fe0e0cacb5f3a7204d3bed6ab8d15f78ba
Kelli Libin <km.putnam2@gmail.com>
2024-01-19 11:08:13 -0700

    add session provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+//how we get information about whether or not the user is signed in, so we can access in client components
+import { SessionProvider } from "next-auth/react";
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 export default function Providers({ children }: ProvidersProps) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
 }
- 
