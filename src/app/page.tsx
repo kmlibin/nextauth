580bf32ae1515655bf1c4a9fc6ee273883888424
Kelli Libin <km.putnam2@gmail.com>
2024-01-23 12:22:27 -0700

    create topic form ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import TopicCreateForm from "@/components/topics/topiccreateform";
+
 export default function Home() {
-  return <div>Home Page</div>
+  return (
+    <div className=" grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top Posts</h1>
+      </div>
+      <div className=""><TopicCreateForm /></div>
+    </div>
+  );
 }
--- src/components/topics/topiccreateform.tsx
+++ src/components/topics/topiccreateform.tsx
@@ -1,3 +1,34 @@
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+
+import * as actions from '../../app/actions'
+
 export default function TopicCreateForm() {
-    return <div>TopicCreateForm</div>
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action = {actions.createComment}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a topic</h3>
+            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Textarea
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Describe Your Topic"
+            />
+            <Button type="submit"> Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
 }
