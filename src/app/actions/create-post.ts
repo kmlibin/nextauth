4f0018ce6ae17c4b07275c042d89d79f320c14de
Kelli Libin <km.putnam2@gmail.com>
2024-01-25 14:33:39 -0700

    creat interface for formstate, start initial validation

--- src/app/actions/create-post.ts
+++ src/app/actions/create-post.ts
@@ -1,6 +1,46 @@
 "use server";
 
-export async function createPost() {
-//revalidate topic show page
+import type { Post } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
 
+//validation object for zod
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+//interface what we will be returning from the createPost
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  if (!result.success) {
+    //make sure return looks like the interface
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  return {
+    errors: {},
+  };
+  //revalidate topic show page
 }
