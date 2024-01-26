3dd73ffd7755d7a1a6d8e82b2e8a756067500a90
Kelli Libin <km.putnam2@gmail.com>
2024-01-26 11:10:51 -0700

    create query to fetch comments by post id

---
+++ src/db/queries/comments.ts
@@ -0,0 +1,15 @@
+import type { Comment } from "@prisma/client";
+import { db } from "@/db";
+
+export type CommentWithAuthor = Comment & {
+  user: { name: string | null; image: string | null };
+};
+
+export function fetchCommentsbyPostId(
+  postId: string
+): Promise<CommentWithAuthor[]> {
+  return db.comment.findMany({
+    where: { postId: postId },
+    include: { user: { select: { name: true, image: true } } },
+  });
+}
