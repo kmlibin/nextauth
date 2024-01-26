f8788d1356ac6751adc01dc7f525949aae653743
Kelli Libin <km.putnam2@gmail.com>
2024-01-26 11:11:22 -0700

    pass prop

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import PostShow from "@/components/posts/post-show";
 import CommentList from "@/components/comments/comment-list";
 import CommentCreateForm from "@/components/comments/comments-create-form";
+import { fetchCommentsbyPostId } from "@/db/queries/comments";
 import paths from "@/paths";
 
 interface PostShowPageProps {
@@ -16,12 +17,15 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
 
   return (
     <div className="space-y-3">
-      <Link className="underline decoration-solid" href={paths.topicShowPath(slug)}>
+      <Link
+        className="underline decoration-solid"
+        href={paths.topicShowPath(slug)}
+      >
         {"< "}Back to {slug}
       </Link>
-      <PostShow postId={postId}/>
+      <PostShow postId={postId} />
       <CommentCreateForm postId={postId} startOpen />
-      {/* <CommentList comments={comments} /> */}
+      <CommentList fetchData={() => fetchCommentsbyPostId(postId)} />
     </div>
   );
 }
