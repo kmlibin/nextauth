e50e5d3c5985d408251e79f74551df96d93078fd
Kelli Libin <km.putnam2@gmail.com>
2024-01-26 10:39:13 -0700

    added a more streamlined example, commented out

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -3,6 +3,22 @@ import { db } from "@/db";
 
 //query file with db requests
 
+//this one is a more streamlined example. 
+// export type PostWithData = Awaited<
+//   ReturnType<typeof fetchPostsByTopicSlug>
+// >[number];
+
+// export function fetchPostsByTopicSlug(slug: string) {
+//   return db.post.findMany({
+//     where: { topic: { slug: slug } },
+//     include: {
+//       topic: { select: { slug: true } },
+//       user: { select: { name: true } },
+//       _count: { select: { comments: true } },
+//     },
+//   });
+// }
+
 export type PostWithData = Post & {
   topic: { slug: string };
   user: { name: string | null };
