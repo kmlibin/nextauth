41953e0198e4ecdc41c86622035efefbc82c6c2f
Kelli Libin <km.putnam2@gmail.com>
2024-01-26 14:31:21 -0700

    add suspense to the search input

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,12 +4,13 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Input,
 } from "@nextui-org/react";
+import { Suspense } from "react";
 
+//must wrap search bar in suspense because it uses useSearchParams. don't need a fallback prop if we don't want
 import HeaderAuth from "./headerauth";
+import SearchInput from "./search-input";
 export default function Header() {
-
   return (
     <Navbar className="shadow mb-6">
       <NavbarBrand>
@@ -19,10 +20,14 @@ export default function Header() {
       </NavbarBrand>
       <NavbarContent justify="center">
         <NavbarItem>
-          <Input />
+          <Suspense>
+            <SearchInput />
+          </Suspense>
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end"><HeaderAuth /></NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   );
 }
